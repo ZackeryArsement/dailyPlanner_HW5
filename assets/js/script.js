880d08cabebba18902ca56d02cf40e887f1d249d
Zackery Arsement <zarsement@hotmail.com>
2022-01-13 18:49:10 -0600

    Javascript is complete. The browser updates every minute to check if the inputs need to change colors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,34 +9,71 @@ date.text(today.format('dddd, MMMM Do'));
 var timeArray = [];
 var time = 9;
 
-var colorArray = [];
-
 // Retrieves stored array or initializes a new one if blank nothing previously stored
 var savedEvents = JSON.parse(localStorage.getItem("savedEvents")) || ["","","","","","","","","",""];
 var buttonText = JSON.parse(localStorage.getItem("buttonText")) || ["","","","","","","","","",""];
 
-checkTime();
+// Keep track of which buttons are pushed down
+var pushedButtons = JSON.parse(localStorage.getItem("activeButtons")) || [null,null,null,null,null,null,null,null,null,null];
+
+// Create an array for all the work day hours
+for(i=0; i<9; i++){
+    timeArray[i] = moment(time, "HH").format("hA");
+    time++;
+}
+
 createSchedule();
 
-console.log(buttonText);
+var inputArray = $('input');
+checkButton();
+setTime();
+
 
 // Past time is gray, present is red, future is green
 function checkTime(){
-    // Create an array for all the work day hours
-    for(i=0; i<9; i++){
-        timeArray[i] = moment(time, "HH").format("hA");
-        time++;
-    }
+    console.log('it has been a minute');
 
     for(i=0; i<9; i++){
+        var listOfClasses = inputArray[i].classList;
+
         if((moment().format("H") - moment(timeArray[i], "hA").format("H")) > 0){
-            colorArray[i] = "bg-secondary"
+            if(listOfClasses.contains('bg-success')){
+                listOfClasses.remove('bg-success');
+                listOfClasses.add('bg-secondary');
+            }
+            else if(listOfClasses.contains('bg-danger')){
+                listOfClasses.remove('bg-danger');
+                listOfClasses.add('bg-secondary');
+            }
+            else if(!listOfClasses.contains('bg-secondary')){
+                listOfClasses.add('bg-secondary');
+            }
         }
-        else if((moment("13", "H").format("H") - moment(timeArray[i], "hA").format("H")) < 0){
-            colorArray[i] = "bg-success"
+        else if((moment().format("H") - moment(timeArray[i], "hA").format("H")) < 0){
+            if(listOfClasses.contains('bg-secondary')){
+                listOfClasses.remove('bg-secondary');
+                listOfClasses.add('bg-success');
+            }
+            else if(listOfClasses.contains('bg-danger')){
+                listOfClasses.remove('bg-danger');
+                listOfClasses.add('bg-success');
+            }
+            else if(!listOfClasses.contains('bg-success')){
+                listOfClasses.add('bg-success');
+            }
         }
         else{
-            colorArray[i] = "bg-danger"
+            if(listOfClasses.contains('bg-success')){
+                listOfClasses.remove('bg-success');
+                listOfClasses.add('bg-danger');
+            }
+            else if(listOfClasses.contains('bg-secondary')){
+                listOfClasses.remove('bg-secondary');
+                listOfClasses.add('bg-danger');
+            }
+            else if(!listOfClasses.contains('bg-danger')){
+                listOfClasses.add('bg-danger');
+            }
         }
     }
 }
@@ -46,7 +83,7 @@ function createSchedule(){
     for(i=0; i<9; i++){
         var newCard = document.createElement('div');
     
-        newCard.innerHTML = "<div class='container w-100 schedule-card'><div class='row w-100'><div class='col-1 border border-left-0 border-dark'>"+ timeArray[i] +"</div><div class='col-9 p-0 m-0' id='input-parent'><input type='text' name='fname' class='w-100 " + colorArray[i] +" border-0' value='" + savedEvents[i] + "'><br></div><button onclick='clickedButton(this)' class='col-1 bg-info rounded-right ml-1 mb-1' id='button" + i + "'>" + buttonText[i] + "</button></div></div>"
+        newCard.innerHTML = "<div class='container w-100 schedule-card'><div class='row w-100'><div class='col-1 border border-left-0 border-dark'>"+ timeArray[i] +"</div><div class='col-9 p-0 m-0' id='input-parent'><input type='text' name='fname' class='w-100 border-0' value='" + savedEvents[i] + "'><br></div><button onclick='clickedButton(this)' class='col-1 bg-info rounded-right ml-1 mb-1' id='button" + i + "'>" + buttonText[i] + "</button></div></div>"
     
         body.append(newCard);
     }
@@ -63,7 +100,7 @@ function clickedButton(elem){
     // Stores what user typed into variable
     var userInput;
 
-    if(elem.innerText === ''){
+    if(elem.innerText === '' && (elem.parentNode.children[1].children[0].value !== '')){
 
         // Save which buttons need to keep the text 'saved'
         elem.innerText = 'Saved';
@@ -80,8 +117,11 @@ function clickedButton(elem){
     
         //saves array as string in localstorage
         localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+
+        pushedButtons.splice(index, 1, elem);
+        localStorage.setItem('activeButtons', JSON.stringify(pushedButtons));
     }
-    else{
+    else if(elem.innerText === 'Saved'){
         // Remove the 'save' text from local storage
         elem.innerText = '';
         saveText = elem.innerText;
@@ -95,18 +135,39 @@ function clickedButton(elem){
         // Creates new array with values meant to be saved
         savedEvents.splice(index, 1, userInput);
     
-        //saves array as string in localstorage
+        // Saves array as string in localstorage
         localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
-    }  
+
+
+        pushedButtons.splice(index, 1, null);
+        localStorage.setItem('activeButtons', JSON.stringify(pushedButtons));
+    }
 }
 
-// Load all the locally saved values and place them into input
-function renderSavedEvents() {
-    for (var i = 0; i<savedEvents.length; i++) {
-        var event = savedEvents[i];
-        console.log(event)
+// Check if save button is pressed when screen loads... if it is pressed and input is empty than unpress the save button
+function checkButton(){
+    var buttonArray = $('button');
 
-        //sets value to what is saved in local storage
-        $('.container').children().eq(i).children('input').attr('value', event)
+    for(i=0; i<buttonArray.length; i++){
+        if((buttonText[i] === 'Saved') && (savedEvents[i] === '')){
+
+            // Remove the 'save' text from local storage
+            buttonArray[i].innerText = '';
+            saveText = '';
+
+            // Creates new array with values meant to be saved
+            buttonText.splice(i, 1, saveText);
+
+            localStorage.setItem('buttonText', JSON.stringify(buttonText));
+        }
     }
+}
+
+// Every minute check the time and change the input colors accordingly
+function setTime(){
+    checkTime();
+
+    var timerInterval = setInterval(function(){
+        checkTime();
+    }, 60000);
 }
